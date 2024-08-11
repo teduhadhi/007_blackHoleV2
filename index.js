e49551a39cc1998df18f19a67a4524802f0b971e
Teduh Adhi <teduhadhi@gmail.com>
2024-08-11 20:19:53 +0700

    border manipulation

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const ringNumber = 33;
+const ringNumber = 30;
 const blackHole = document.querySelector(".black-hole");
 
 // const ringArray = Array.from({length:ringNumber}, () => 0)
@@ -9,18 +9,40 @@ const degreeToRad = (angle) => angle * Math.PI / 180;
 for (let i = 0.; i <= ringNumber; i++){
 
   let halfMark = ringNumber/2
-  let x = 100 + Math.pow(i,2) * 3;
-  let y = i * 20;
+  let x = 200 + Math.pow(i,2.3) * 10;
+  let y = i * 50;
 
+  if( i <= halfMark){
   const ringCircle = document.createElement("div");
 
-  ringCircle.classList.add("ring-circle");
+  ringCircle.classList.add("ring-circle-1");
   ringCircle.style.setProperty("--radius", `${x}px`);
   ringCircle.style.setProperty("--height", `${y}px`);
+  ringCircle.style.setProperty("--shadow", `${i*5}px`);
   ringCircle.style.setProperty("zindex", `${i}`);
-  ringCircle.style.setProperty("opacity", `${(i/ringNumber)*2}`);
-  ringCircle.style.setProperty("--borderradius", `${(i/ringNumber)*5}px`);
-
+  ringCircle.style.setProperty("opacity", `${(1/i)*2}`);
+  ringCircle.style.setProperty("--borderradius", `${(i/ringNumber)*15}px`);
   blackHole.appendChild(ringCircle);
+  } else {
+    let newIndex = i - halfMark
+    x = 200 + Math.pow(newIndex,2.3) * 10;
+    y = newIndex * 50;
+
+
+    const ringCircle = document.createElement("div");
+
+    ringCircle.classList.add("ring-circle-2");
+    ringCircle.style.setProperty("--radius", `${x}px`);
+    ringCircle.style.setProperty("--height", `${-y}px`);
+    ringCircle.style.setProperty("--shadow", `${newIndex*5}px`);
+    ringCircle.style.setProperty("zindex", `${-newIndex}`);
+    ringCircle.style.setProperty("opacity", `${(1/newIndex)*2}`);
+    ringCircle.style.setProperty("--borderradius", `${(newIndex/ringNumber)*5}px`);
+    
+    blackHole.appendChild(ringCircle);
+  }
+  
+
+ 
 
 }
